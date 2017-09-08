dc08b917d94d5d1c6069bddbc54d9bebe9bbbfb9
dlwaysi <dlwaysi@miltronicsinc.com>
2017-09-08 01:06:28 +0000

    Added Fourth Solution to Multiples_3_5, Arithmetic Progression

---
+++ Multiples_3_5/Mult_35_Arith_prog.py
@@ -0,0 +1,17 @@
+#Project Euler
+#Problem # 1
+#Task: Calculate the sum of all of the multiples of 3 and 5 below 1000
+#Author: Daniel Lwaysi
+#Note: This is my fourth solution which uses the Arithmetic Progression Technique
+#Date: 9/7/2017
+
+n         = 1000
+multiples = (3,5,15)
+
+nterms    = [(n-1)//c for c in multiples]
+m35sums   = [d*c*(1+d)/2 for d,c in zip(nterms, multiples)]
+
+tsum      = m35sums[0] + m35sums[1] - m35sums[2]
+
+print(tsum)
+
